b47143ee6f3f9bf6a2faa79125888bf6a318811b
rzambrano1 <rzambrano@gmail.com>
2025-11-20 18:49:45 -0500

    crate_rename

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,7 @@ use std::{
     thread,
     time::Duration
 };
-use hello::ThreadPool;
+use multithreaded_server_example::ThreadPool;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
