4b7344767f7739a325196589bb3368ff7586e014
rzambrano1 <rzambrano@gmail.com>
2025-11-20 18:39:57 -0500

    multi_threading_server_example

---
+++ src/main.rs
@@ -0,0 +1,96 @@
+// Example from "The Rust Programming Language"
+// By Klabnik and Nichols [Thank you for your awesome book :-)]
+
+use std::{
+    fs,
+    io::{prelude::*, BufReader},
+    net::{TcpListener, TcpStream},
+    thread,
+    time::Duration
+};
+use hello::ThreadPool;
+
+fn main() {
+    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+    let pool = ThreadPool::new(4);
+
+    for stream in listener.incoming().take(2) {
+        let stream = stream.unwrap();
+
+        //println!("Connection established!");
+        
+        // Infinite threads
+        // thread::spawn(|| {
+        //     handle_connection(stream);
+        // });
+
+        // Finite pool of threads
+        pool.execute(|| {
+            handle_connection(stream);
+        })
+    }
+
+    println!("Shutting down the server!")
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let buf_reader = BufReader::new(&mut stream);
+    
+    // let http_request: Vec<_> = buf_reader
+    //     .lines()
+    //     .map(|result| result.unwrap())
+    //     .take_while(|line| !line.is_empty())
+    //     .collect();
+    //println!("Request: {:#?}", http_request);
+
+    let request_line = buf_reader
+        .lines()
+        .next()
+        .unwrap()
+        .unwrap();
+    
+    // Second Version
+    // --------------
+    // let status_line = "HTTP/1.1 200 OK";
+    // let contents = fs::read_to_string("hello.html").unwrap();
+    // let length = contents.len();
+
+    // let response = format!(
+    //     "{status_line}\r\n\
+    //     Content-Length: {length}\r\n\r\n
+    //     {contents}"
+    // );
+    // stream.write_all(response.as_bytes()).unwrap()
+    // --------------
+
+    // Third Version
+    // -------------
+    // let (status_line,filename) = 
+    //     if request_line == "GET / HTTP/1.1" {
+    //         ("HTTP/1.1 200 OK", "hello.html")
+    //     } else {
+    //         ("HTTP/1.1 404 NOT FOUND", "404.html")
+    //     };
+    
+    // Forth Version
+    // -------------
+    let (status_line,filename) = match &request_line[..] {
+        "GET / HTTP/1.1" => ("HTTP/1.1 200 OK", "hello.html"),
+        "GET /sleep HTTP/1.1" => {
+            thread::sleep(Duration::from_secs(15));
+            ("HTTP/1.1 200 OK", "hello.html")
+        }
+        _ => ("HTTP/1.1 404 NOT FOUND", "404.html"),
+    };
+
+    let contents = fs::read_to_string(filename).unwrap();
+    let length = contents.len();
+
+    let response = format!(
+        "{status_line}\r\n\
+        Content-Length: {length}\r\n\r\n
+        {contents}"
+    );
+
+    stream.write_all(response.as_bytes()).unwrap()
+}
